9afcdd88b61abc8943d048da7b792ee6bc647b94
ayoublahdoud <ayoub.lahdoud@e-polytechnique.ma>
2024-08-17 02:12:48 +0100

    Mise à jour pour déploiement

--- services/api.js
+++ services/api.js
@@ -1,6 +1,3 @@
-// services/api.js
-
-// API pour récupérer les données de regle agrégées
 export const fetchRegleData = async () => {
   const response = await fetch('https://tranquil-shelf-72645-6e0212cb96fc.herokuapp.com/dashboard/api/aggregated_regle/');
   if (!response.ok) {
@@ -9,7 +6,6 @@ export const fetchRegleData = async () => {
   return await response.json();
 };
 
-// API pour récupérer les données de regle
 export const fetchRegleDataAlternative = async () => {
   const response = await fetch('https://tranquil-shelf-72645-6e0212cb96fc.herokuapp.com/dashboard/api/regle/');
   if (!response.ok) {
@@ -18,7 +14,6 @@ export const fetchRegleDataAlternative = async () => {
   return await response.json();
 };
 
-// API pour récupérer les données de stock
 export const fetchStockData = async () => {
   const response = await fetch('https://tranquil-shelf-72645-6e0212cb96fc.herokuapp.com/dashboard/api/stock/');
   if (!response.ok) {
