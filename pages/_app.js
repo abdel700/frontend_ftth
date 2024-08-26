2c46bce1653cdf6d58fab4711565d25986c9a094
ayoublahdoud <ayoub.lahdoud@e-polytechnique.ma>
2024-08-26 11:15:10 +0100

    Update PWA

--- pages/_app.js
+++ pages/_app.js
@@ -4,16 +4,20 @@ import '../styles/globals.css';
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <title>FTTH Dashboard</title>
-        <link rel="icon" href="/images/ftth_logo.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta property="og:title" content="FTTH Dashboard - INTELCIA IT SOLUTION" />
-        <meta property="og:description" content="Plateforme de gestion des problèmes FTTH" />
-        <meta property="og:image" content="/images/ftth_logo.png" />
-        <meta property="og:url" content="https://ftth-dashboard.vercel.app/" />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
+<Head>
+  <title>FTTH Dashboard</title>
+  <link rel="icon" href="/images/ftth_logo.png" />
+  <link rel="manifest" href="/manifest.json" />
+  <meta name="apple-mobile-web-app-capable" content="yes" />
+  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+  <meta name="theme-color" content="#1E40AF" />
+  <meta property="og:title" content="FTTH Dashboard - INTELCIA IT SOLUTION" />
+  <meta property="og:description" content="Plateforme de gestion des problèmes FTTH" />
+  <meta property="og:image" content="/images/ftth_logo.png" />
+  <meta property="og:url" content="https://ftth-dashboard.vercel.app/" />
+  <meta name="twitter:card" content="summary_large_image" />
+</Head>
+
       <Component {...pageProps} />
     </>
   );
