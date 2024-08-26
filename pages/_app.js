cef566ff46e832e8e991113f42451266c0710e57
ayoublahdoud <ayoub.lahdoud@e-polytechnique.ma>
2024-08-26 11:05:01 +0100

    Update TopReglesDetails component and fix report

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>FTTH Dashboard</title>
+        <link rel="icon" href="/images/ftth_logo.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta property="og:title" content="FTTH Dashboard - INTELCIA IT SOLUTION" />
+        <meta property="og:description" content="Plateforme de gestion des problèmes FTTH" />
+        <meta property="og:image" content="/images/ftth_logo.png" />
+        <meta property="og:url" content="https://ftth-dashboard.vercel.app/" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
