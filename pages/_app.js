4c3b8d3c0e23db18cfa5514eba71a6bebec3aab7
ayoublahdoud <ayoub.lahdoud@e-polytechnique.ma>
2024-08-26 11:30:12 +0100

    Update Front

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,12 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>INTELCIA IT SOLUTION - FTTH Dashboard</title>
+        <title>FTTH Dashboard</title>
         <link rel="icon" href="/images/ftth_logo.png" />
         <link rel="manifest" href="/manifest.json" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="theme-color" content="#0000ff" />
         <meta property="og:title" content="FTTH Dashboard - INTELCIA IT SOLUTION" />
         <meta property="og:description" content="Plateforme de gestion des problèmes FTTH" />
         <meta property="og:image" content="/images/ftth_logo.png" />
