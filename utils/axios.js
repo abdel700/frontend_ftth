08e53279219e2146a5ed00c09a3af460952b5df3
ayoublahdoud <ayoub.lahdoud@e-polytechnique.ma>
2024-08-31 16:01:30 +0100

    Added dj-ddase-url

--- utils/axios.js
+++ utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'https://ftth-authentification-100be1e354a5.herokuapp.com/api/', // Remplacez par l'URL de votre API avec un slash à la fin
-  timeout: 1000,
+  timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
   },
